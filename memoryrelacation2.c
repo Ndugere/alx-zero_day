3e4cd3bba7746e689cfc271ecf47186bd364a687
Ndugere <ndungudavid972@gmail.com>
2023-03-31 16:23:59 -0700

    realocating memory using realloc fuction

---
+++ memoryrelacation2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+        int *list =  malloc(3 * sizeof(int));
+        if ( list == NULL)
+        {
+                printf("Not enough memory");
+                free(list);
+                return 1;
+        }
+        list[0] = 1;
+        list[1] = 2;
+        list[2] = 3;
+        for (int i = 0; i < 3; i++)
+        {
+                printf(" The initial ints coresponding to the int saved in the initial memory alocated %i\n", list[i]);
+        }
+        //Time passes and you realize you need more memory
+        int *tmp = realloc(list, (4 * sizeof(int)));
+        if (tmp == NULL)
+        {
+                printf("Not enough memory");
+                free(tmp);
+                return 1;
+        }
+        tmp[3] = 4;
+        list = tmp;
+        for (int i = 0; i < 4; i++)
+        {
+                printf("final ints coresponding to the new size of memory block with integers %i\n", list[i]);
+        }
+        free(list);
+        return 0;
+}
